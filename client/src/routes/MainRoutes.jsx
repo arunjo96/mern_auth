1be4bc07ee8b14a818abc1c8dae1493e74581cf8
arunjo96 <arunkumarvadivel96@gmail.com>
2026-06-02 12:26:43 +0530

    Remove unused CSS import and simplify route imports in MainRoutes

--- client/src/routes/MainRoutes.jsx
+++ client/src/routes/MainRoutes.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Profile from '../pages/Profile';
-import ForgotPassword from '../pages/Forgot';
-import ResetPassword from '../pages/Reset';
+import Forgot from '../pages/Forgot';
+import Reset from '../pages/Reset';
 
 const MainRoutes = () => {
   return (
@@ -14,8 +14,8 @@ const MainRoutes = () => {
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route path="/forgot-password" element={<Forgot />} />
+          <Route path="/reset-password/:token" element={<Reset />} />
         </Routes>
       </Router>
     </>
